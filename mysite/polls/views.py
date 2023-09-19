daee6f7e97a6ba79293e95ef4bcf96b75203a0d2
leon-rdo <leonardoabreu1212@gmail.com>
2023-09-19 11:59:45 -0300

    refactor: estiliza views e troca def vote() pelo método post

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,37 +1,43 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
-from django.urls import reverse
+from django.shortcuts import redirect
+from .models import Question
 from django.views import generic
 from django.utils import timezone
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
-    
+   
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
     
-
+    def post(self, request, *args, **kwargs):
+        question = self.get_object()
+        choice_id = request.POST.get('choice')
+        selected_choice = question.choice_set.get(pk=choice_id)
+        selected_choice.votes += 1
+        selected_choice.save()
+                
+        return redirect('resultados', question.id)
+    
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
+"""
+Tradicional Way of Doing the Views:
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -52,20 +58,20 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-#   Tradicional Way of Doing the Views:
 
-#def index(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #context = {'latest_question_list': latest_question_list}
-    #return render(request, 'polls/index.html', context)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-#def detail(request, question_id):
-    #question = get_object_or_404(Question, pk=question_id)
-    #return render(request, 'polls/detail.html', {'question': question})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
-#def results(request, question_id):
-    #question = get_object_or_404(Question, pk=question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
 
-#def results(request, question_id):
-    #question = get_object_or_404(Question, pk=question_id)
-    #return render(request, 'polls/results.html', {'question': question})
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+"""
